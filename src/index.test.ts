bd81f3e10bba70bcd08a0b1eabf450643831e76f
Mikolaj Duchewicz <mikolaj.duchewicz@shelf.io>
2022-10-28 13:41:57 +0200

    feat: [REACT-341] add src code and tets

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,27 @@
-import {getFoo} from './index';
+import getVideoId from './index';
 
-it('should return bar', () => {
-  expect(getFoo()).toEqual('bar');
+describe('getVideoId', () => {
+  it('should return embded url youtube videos', function () {
+    const meta = getVideoId('https://youtu.be/LqTuxFElqag');
+
+    expect(meta.service).toEqual('youtube');
+    expect(meta.id).toEqual('LqTuxFElqag');
+    expect(meta.embedURL).toEqual('https://www.youtube.com/embed/LqTuxFElqag');
+  });
+
+  it('should return embded with start time params for youtube videos', function () {
+    const meta = getVideoId('https://youtu.be/LqTuxFElqag?t=7939');
+
+    expect(meta.service).toEqual('youtube');
+    expect(meta.id).toEqual('LqTuxFElqag');
+    expect(meta.embedURL).toEqual('https://www.youtube.com/embed/LqTuxFElqag?start=7939');
+  });
+
+  it('should return embded url for vimeo youtube videos', function () {
+    const meta = getVideoId('https://vimeo.com/259411563#t=50s');
+
+    expect(meta.service).toEqual('vimeo');
+    expect(meta.id).toEqual('259411563');
+    expect(meta.embedURL).toEqual('https://player.vimeo.com/video/259411563');
+  });
 });
